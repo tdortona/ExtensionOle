ec0cae2e610d3d0492d161b1114825fcb56fd344
Maximiliano D'Amelio <maximilianodamelio@gmail.com>
2023-07-04 11:02:36 -0300

    mejoras + icono

--- contentScript.js
+++ contentScript.js
@@ -1,47 +1,48 @@
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    if (request.action === 'showBoca') {
-      let articles = document.querySelectorAll('article div h3.volanta');
-      const display = 'block';
-      for (let article of articles) {
-        if (article.textContent === 'BOCA ' || article.textContent === 'BOCA') {
-          let divs = article.closest('[class^="div"]').querySelectorAll('div');
-          for (let div of divs) {
-            div.style.display = display;
-          }
-        }
-      }
-    } else if (request.action === 'hideBoca') {
-      let articles = document.querySelectorAll('article div h3.volanta');
-      const display = 'none';
-      for (let article of articles) {
-        if (article.textContent === 'BOCA ' || article.textContent === 'BOCA') {
-          let divs = article.closest('[class^="div"]').querySelectorAll('div');
-          for (let div of divs) {
-            div.style.display = display;
-          }
-        }
-      }
-    } else if (request.action === 'showRiver') {
-      let articles = document.querySelectorAll('article div h3.volanta');
-      const display = 'block';
-      for (let article of articles) {
-        if (article.textContent === 'RIVER ' || article.textContent === 'RIVER') {
-          let divs = article.closest('[class^="div"]').querySelectorAll('div');
-          for (let div of divs) {
-            div.style.display = display;
-          }
-        }
-      }
-    } else if (request.action === 'hideRiver') {
-        let articles = document.querySelectorAll('article div h3.volanta');
-        const display = 'none';
-        for (let article of articles) {
-            if (article.textContent === 'RIVER ' || article.textContent === 'RIVER') {
-              let divs = article.closest('[class^="div"]').querySelectorAll('div');
-              for (let div of divs) {
-                div.style.display = display;
-              }
-            }
-          }
+  let articles = document.querySelectorAll('article div h3.volanta');
+  let articlesBoca = [];
+  let articlesRiver = [];
+  
+  for (let article of articles) {
+    if (article.textContent === 'BOCA ' || article.textContent === 'BOCA') {
+      articlesBoca.push(article);
+    } else if (article.textContent === 'RIVER ' || article.textContent === 'RIVER') {
+      articlesRiver.push(article)
     }
+  }
+
+  if (request.action === 'showBoca') {
+    for (let article of articlesBoca) {
+
+      let divs = article.closest('[class^="div"]').querySelectorAll('div');
+      console.log(divs);
+      for (let div of divs) {
+        div.style.cssText = div.style.cssText.replace('display: none;', '')
+      }
+    }
+  } else if (request.action === 'hideBoca') {
+    const display = 'none';
+    for (let article of articlesBoca) {
+      let divs = article.closest('[class^="div"]').querySelectorAll('div');
+
+      for (let div of divs) {
+        div.style.display = display;
+      }
+    }
+  } else if (request.action === 'showRiver') {
+    for (let article of articlesRiver) {
+      let divs = article.closest('[class^="div"]').querySelectorAll('div');
+      for (let div of divs) {
+        div.style.cssText = div.style.cssText.replace('display: none;', '')
+      }
+    }
+  } else if (request.action === 'hideRiver') {
+    const display = 'none';
+    for (let article of articlesRiver) {
+      let divs = article.closest('[class^="div"]').querySelectorAll('div');
+      for (let div of divs) {
+        div.style.display = display;
+      }
+    }
+  }
 });
